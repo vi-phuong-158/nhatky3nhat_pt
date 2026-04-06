25f0857a26a751fdca8f6d8eb7d926ee5e33bbad
vi-phuong-158 <anmphongandn@gmail.com>
2026-04-07 00:29:18 +0700

    chore: set vite base path for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/nhatky3nhat_pt/', // Cấu hình đường dẫn cho GitHub Pages
 })
